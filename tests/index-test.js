46ad4f603ba11984e595b436806dd3f600f61409
Patrick Neel <patrick.neel@colorado.edu>
2017-10-23 14:45:53 -0600

    added es6 import and export functionality

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,19 @@
 import { expect } from 'chai';
 import index from '../lib/index'
 import { bubbleSort, insertionSort, mergeSort, quickSort } from '@patneel87/sorting-suite';
+import { trie } from '../lib/Trie.js';
+import { node } from '../lib/Node.js';
+
 
 describe('example test file', () => {
   it('should sort some stuff', () => {
     expect(bubbleSort([3, 2, 1])).to.deep.equal([1, 2, 3]);
+  })
+})
+
+
+describe('should describe Trie', () => {
+  it('should do some stuff', () => {
+    expect(trie).is.object;
   })
 })
