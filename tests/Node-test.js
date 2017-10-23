3d59a62c667524085e2f65dd556b4996f1a9cf37
Patrick Neel <patrick.neel@colorado.edu>
2017-10-23 15:09:21 -0600

    testing set up

---
+++ tests/Node-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { Node } from '../lib/Node.js';
+
+
+describe('Node', () => {
+
+  it.skip('should be a ', () => {
+    expect(Node).to.be.a('object');
+
+  });
+
+});
---
+++ tests/Trie-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { Trie } from '../lib/Trie.js';
+import { Node } from '../Node.js';
+
+describe('Trie', () => {
+
+  it.skip('it should be ', () => {
+    expect(Trie).to.be.a('object');
+  })
+
+});
