083f41ca0d324db4d29ccdee9a0cd2414b3372c8
Patrick Neel <patrick.neel@colorado.edu>
2017-10-24 15:08:49 -0600

    node test

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -6,22 +6,22 @@ import fs from 'fs';
 const text = "/usr/share/dict/words"
 const dictionary = fs.readFileSync(text).toString().trim().split('\n')
 
-let completion;
+let trie;
 
 beforeEach(() => {
-  completion = new Trie();
+  trie = new Trie();
 });
 
 describe('Trie', () => {
 
   it('it should be an object', () => {
-    expect(completion).to.be.an('object');
+    expect(trie).to.be.an('object');
 
   });
 
   it('should have a node as the root property', () => {
     let node = new Node('');
-    expect(completion.root).to.deep.equal(node);
+    expect(trie.root).to.deep.equal(node);
 
   });
 
@@ -30,39 +30,39 @@ describe('Trie', () => {
 describe('Insert', () => {
 
   it('should be able to insert a word', () => {
-    completion.insert('corgi');
-    // console.log(JSON.stringify(completion, null, 2))
+    trie.insert('corgi');
+    // console.log(JSON.stringify(trie, null, 2))
 
   });
 
   it('should be able to insert many words', () => {
-    completion.insert('stuff');
-    completion.insert('things');
-    completion.insert('cobra');
+    trie.insert('stuff');
+    trie.insert('things');
+    trie.insert('cobra');
 
   });
 
   it('should be able to count a word', () => {
-    expect(completion.count).to.equal(0);
-    completion.insert('corgi');
-    expect(completion.count).to.equal(1);
+    expect(trie.count).to.equal(0);
+    trie.insert('corgi');
+    expect(trie.count).to.equal(1);
 
   });
 
   it('should be able to count many words', () => {
-    expect(completion.count).to.equal(0);
-    completion.insert('cobra');
-    expect(completion.count).to.equal(1);
-    completion.insert('gazelle');
-    expect(completion.count).to.equal(2);
-    completion.insert('snake');
-    expect(completion.count).to.equal(3);
+    expect(trie.count).to.equal(0);
+    trie.insert('cobra');
+    expect(trie.count).to.equal(1);
+    trie.insert('gazelle');
+    expect(trie.count).to.equal(2);
+    trie.insert('snake');
+    expect(trie.count).to.equal(3);
 
   });
 
   it('should populate the dictionary', () => {
-    completion.populate(dictionary);
-    expect(completion.count).to.equal(235886);
+    trie.populate(dictionary);
+    expect(trie.count).to.equal(235886);
 
   });
 
